ff4bbb71b2a6a67b497260bf956838d11d76841f
Stoyan Rizov <stoyanrizov6@gmail.com>
2019-03-06 19:36:04 +0000

    fixed compiling errors,program now executes

--- sha256.c
+++ sha256.c
@@ -7,13 +7,7 @@
 // For using fixed bit lenght integer.
 #include <stdint.h>
 
-void sha256(){
-
-  // The K constants.
-  uint32_t K[] = { 
-
-
-}
+void sha256();
 
 //See 4.2.2 for definitions
 uint32_t sig0(uint32_t x);
@@ -24,7 +18,7 @@ uint32_t shr(uint32_t n, uint32_t x);
 
 //See Section 4.1.2 for definitions
 uint32_t SIG0(uint32_t x);
-uint32_t SIG1(uint32_t x)l
+uint32_t SIG1(uint32_t x);
 
 //See Section 4.1.2 for definitions
 uint32_t Ch(uint32_t x, uint32_t y, uint32_t z);
@@ -34,16 +28,36 @@ int main(int argc, char *argv[]){
 
   sha256();
        
-   return 0:
+   return 0;
 }
 
-void sha256() { 
+void sha256(){ 
+   // The K constants, defined in Section 4.2.2.
+  uint32_t K[] = {
+
+0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 
+0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 
+0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 
+0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 
+0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 
+0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 
+0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 
+0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 
+0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+
+  };
+       
   //Message schedule (Section 6.2).
   uint32_t W[64];
   //Working variables (Section 6.3).
   uint32_t a,b,c,d,e,f,g,h;
-  //The Hash value (Section 6.2). 
-  uint32_t H[8];
   //Two temporary variables (Section 6.2).
   uint32_t T1, T2;
 
@@ -60,10 +74,13 @@ void sha256() {
    , 0x5be0cd19
   };
 
-  uint32_t M[16];
+  uint32_t M[16] = {0, 0, 0, 0, 0, 0, 0, 0};
 
   //For looping
-  int t;
+  int i, t;
+
+  //Loop through the message blocks as per page 22.
+  for (i = 0; i< 1; i++) {
   
   //From page 22, W[t] = M[t] for 0<= t <= 15.
   for (t = 0; t < 16; t++)
@@ -89,7 +106,7 @@ void sha256() {
     c = b;
     b = a;
     a = T1 + T2;
-
+  }
   //Step 4 
   H[0] = a + H[0];
   H[1] = b + H[1];
@@ -100,12 +117,12 @@ void sha256() {
   H[6] = g + H[6];
   H[7] = h + H[7];
 
-
-
+  }
+  printf("%x %x %x %x %x %x %x %x\n", H[0], H[1], H[2], H[3], H[4], H[5], H[6], H[7]);
 
   }
 
-}
+
 
 uint32_t rotr(uint32_t n, uint32_t x){
   return(x >> n) | (x << (32-n));
