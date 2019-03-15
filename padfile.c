f1d810cec33020d918efafb6374469751da4c2f9
Stoyan Rizov <stoyanrizov6@gmail.com>
2019-03-15 17:06:59 +0000

    fixed and completed padding

--- padfile.c
+++ padfile.c
@@ -7,6 +7,7 @@ union msgblock {
   uint64_t s[8];
 };
 
+enum status {READ, PAD0, PAD1, FINISH};
 
 int main(int argc, char *argv[]) {
   
@@ -16,15 +17,17 @@ int main(int argc, char *argv[]) {
   
   uint64_t nobytes;
  
+  enum status S = READ; 
 
   FILE* f;
 
   f = fopen(argv[1], "r");
 //checking stdarc (error checking) to do
+  int i;
 
-  while (!feof(f)) {
+  while (S == READ) {
    nobytes = fread(M.e, 1, 64, f);
-   printf ("Read %2llu bytes\n", nobytes);
+   printf("Read %2llu bytes\n", nobytes);
    nobits = nobits + (nobytes * 8);
    if (nobytes < 56) {
    printf("I've found a block with less than 55 bytes!\n");
@@ -34,8 +37,28 @@ int main(int argc, char *argv[]) {
       M.e[nobytes] = 0x00;     
      }
     M.s[7] = nobits; 
-   }
+    S = FINISH;
+   } else if (nobytes < 64){
+     S = PAD0;
+     M.e[nobytes] = 0x80;
+     while (nobytes < 64) {
+     nobytes = nobytes + 1;
+     M.e[nobytes] = 0x00;
+     }     
+   } else if(feof(f)) {
+    S = PAD1;              
+    }
  }
+  
+
+  if (S == PAD0 || S== PAD1) {
+  for (i = 0; i < 56; i++)
+    M.e[i] = 0x00;
+  M.s[7] = nobits;
+  }
+  if (S == PAD1)
+    M.e[0] = 0x80; 
+  
 
   fclose(f); 
 
